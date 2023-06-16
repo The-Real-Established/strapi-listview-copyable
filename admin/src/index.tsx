4d77466ab360f06cb47a89c3e0dddaa23a2f227a
Alexander Engel <ae94@icloud.com>
2023-06-16 14:49:46 -0700

    fix: added regsiter emthod

--- admin/src/index.tsx
+++ admin/src/index.tsx
@@ -1,6 +1,18 @@
 import { INJECT_COLUMN_IN_TABLE } from "@strapi/admin/admin/src/exposedHooks"
 import { injectCopyActionColumns } from "./components/injectCopyActionColumns";
+import * as pluginPkg from "../../package.json";
+import { StrapiAdminInstance } from 'strapi-typed'
+const { name } = pluginPkg.strapi;
+export const pluginId = name.replace(/^strapi-plugin-/i, "");
+
 export default {
+	register(app: StrapiAdminInstance) {
+		app.registerPlugin({
+			id: pluginId,
+			isReady: false,
+			name,
+		});
+	},
 	bootstrap(app: any) {
 		app.registerHook(INJECT_COLUMN_IN_TABLE, injectCopyActionColumns);
 	}
